62178c5e5ddb79fe8ea267ac81ce5848d387a618
HsienYu <chenghsienyu@aim.com>
2021-08-10 00:50:56 +0800

    update the scrolling to the end for fetching all images

--- scrapead.py
+++ scrapead.py
@@ -21,6 +21,8 @@ parser.add_argument("-l", "--location", type=str,
                     required=True, help="country or location ex. TW")
 parser.add_argument("-i", "--max_iterations", type=int,
                     required=True, help="max_iterations counting times")
+# parser.add_argument("-m", "--manual_control", type=bool,
+#                     required=True, help="switch mannual  or automode")
 # parser.add_argument("-cq", "--colorQuality", type=int,
 #                     required=True, help="quality settings, 1 is the highest quality, the bigger the number, the faster the palette generation, but the greater the likelihood that colors will be missed.")
 # parser.add_argument("-cp", "--colorPalette", type=int,
@@ -32,20 +34,47 @@ args = parser.parse_args()
 keyword = args.keyword
 location = args.location
 max_iterations = args.max_iterations
+# manual_control = args.manual_control
 
 # url of fb ads library
 url = "https://www.facebook.com/ads/library/?active_status=all&ad_type=all&country=" + location+"&q="+keyword + \
     "&sort_data[direction]=desc&sort_data[mode]=relevancy_monthly_grouped&search_type=keyword_unordered&media_type=all"
 
 # define webdriver
-options = FirefoxOptions()
-options.add_argument("--headless")
-driver = webdriver.Firefox(options=options)
+# if manual_control is True:
+#     driver = webdriver.Firefox()
+# else:
+#     options = FirefoxOptions()
+#     options.add_argument("--headless")
+#     driver = webdriver.Firefox(options=options)
+driver = webdriver.Firefox()
 driver.get(url)
+
 
 # main action
 
 iterations = 0
+
+
+def scroll(driver, timeout):
+    scroll_pause_time = timeout
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to bottom
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(scroll_pause_time)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            # If heights are the same it will exit the function
+            break
+        last_height = new_height
 
 
 # def appendList(entry):
@@ -60,7 +89,7 @@ iterations = 0
 
 def detect_color(filePath, cp):
     color_thief = ColorThief(filePath)
-    #dominant_color = color_thief.get_color(quality=cq)
+    # dominant_color = color_thief.get_color(quality=cq)
     palette_color = color_thief.get_palette(color_count=5)
     c_arr = []
     for c in palette_color:
@@ -78,6 +107,14 @@ def rgbTohex(c):
 
 
 while iterations < max_iterations:
+    # if manual_control is True:
+    #     pass
+    # else:
+    #     scroll(driver, 3)
+    #     print('fetching as many as possible contents in 10s each, this may takes a while!')
+    scroll(driver, 3)
+    print("fetching as many as possible contents in 10s each, this may takes a while!")
+
     html = driver.execute_script("return document.documentElement.outerHTML")
     sel_soup = bSoup(html, 'html.parser')
     # print(sel_soup.findAll('img'))
@@ -105,16 +142,16 @@ while iterations < max_iterations:
             img_r = requests.get(img, stream=True)
             new_path = os.path.join(
                 current_path, 'images', result_name)
-            # dict = {'keyword': keyword, 'url': img,
-            #         'filePath': new_path, 'date': currentDate}
+            dict = {'keyword': keyword, 'url': img,
+                    'filePath': new_path, 'date': currentDate}
             with open(new_path, 'wb') as output_file:
                 output_file.write(img_r.content)
-                # detectedColor = detect_color(dict["filePath"], 5)
-                # dict['color'] = detectedColor
-                # print(dict)
+                detectedColor = detect_color(dict["filePath"], 5)
+                dict['color'] = detectedColor
+                print(dict)
 
             del img_r
         except:
             pass
     iterations += 1
-    time.sleep(10)
+    time.sleep(5)
