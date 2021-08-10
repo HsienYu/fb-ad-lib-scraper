f4c938688fbbfa4c42cca41cb2f78a20fa997e0a
HsienYu <chenghsienyu@aim.com>
2021-08-10 15:29:04 +0800

    add ignore error

--- scrapead.py
+++ scrapead.py
@@ -5,6 +5,8 @@ import asyncio
 import datetime
 import requests
 import argparse
+import asyncio
+import threading
 import json
 from bs4 import BeautifulSoup as bSoup
 from colorthief import ColorThief
@@ -21,12 +23,7 @@ parser.add_argument("-l", "--location", type=str,
                     required=True, help="country or location ex. TW")
 parser.add_argument("-i", "--max_iterations", type=int,
                     required=True, help="max_iterations counting times")
-# parser.add_argument("-m", "--manual_control", type=bool,
-#                     required=True, help="switch mannual  or automode")
-# parser.add_argument("-cq", "--colorQuality", type=int,
-#                     required=True, help="quality settings, 1 is the highest quality, the bigger the number, the faster the palette generation, but the greater the likelihood that colors will be missed.")
-# parser.add_argument("-cp", "--colorPalette", type=int,
-#                     required=True, help="the size of the palette, max number of colors")
+
 args = parser.parse_args()
 
 # print(args)
@@ -41,12 +38,14 @@ url = "https://www.facebook.com/ads/library/?active_status=all&ad_type=all&count
     "&sort_data[direction]=desc&sort_data[mode]=relevancy_monthly_grouped&search_type=keyword_unordered&media_type=all"
 
 # define webdriver
-# if manual_control is True:
-#     driver = webdriver.Firefox()
-# else:
-#     options = FirefoxOptions()
-#     options.add_argument("--headless")
-#     driver = webdriver.Firefox(options=options)
+'''
+if manual_control is True:
+    driver = webdriver.Firefox()
+else:
+    options = FirefoxOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options)
+'''
 driver = webdriver.Firefox()
 driver.get(url)
 
@@ -107,27 +106,23 @@ def rgbTohex(c):
 
 
 while iterations < max_iterations:
-    # if manual_control is True:
-    #     pass
-    # else:
-    #     scroll(driver, 3)
-    #     print('fetching as many as possible contents in 10s each, this may takes a while!')
     scroll(driver, 5)
-    print("fetching as many as possible contents in 10s each, this may takes a while!")
+    #print("fetching as many as possible contents in 10s each, this may takes a while!")
 
     html = driver.execute_script("return document.documentElement.outerHTML")
     sel_soup = bSoup(html, 'html.parser')
     # print(sel_soup.findAll('img'))
     images = []
     for i in sel_soup.findAll('img'):
-        src = i['src']
-        # images.append(src)
-        # quick remove low resolutiona & wrong image by string find
-        if src.find("s60x60") != -1 or src.find("p100x100") != -1 or src.find("KYEwFe_bozl") != -1 or src.find("hsts-pixel") != -1:
+        try:
+            src = i['src']
+            if src.find("s60x60") != -1 or src.find("p100x100") != -1 or src.find("KYEwFe_bozl") != -1 or src.find("hsts-pixel") != -1:
+                pass
+            else:
+                # print(src)
+                images.append(src)
+        except:
             pass
-        else:
-            # print(src)
-            images.append(src)
 
     current_path = os.getcwd()
     currentDT = datetime.datetime.now()
